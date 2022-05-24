5128ecc0b09ed9fb99ae30c47dbb34dc8228e7b9
Erikas <itserikasbykovskis@gmail.com>
2022-05-24 23:03:47 +0300

    Small initial server setup update

--- server/controller/articleController.js
+++ server/controller/articleController.js
@@ -1,6 +1,8 @@
+// Controller - controls the queries and functionality of a page
+
 exports.homepage = async (req, res) => {
     try {
-        res.render('index');
+        res.render('index', { title: 'SEB Workers - Homepage' });
     } catch (error) {
         console.log(error);
     }
