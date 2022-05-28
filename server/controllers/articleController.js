64eb28dc210b3869f0d4bc48a6505f4f4415be17
Erikas <itserikasbykovskis@gmail.com>
2022-05-28 21:45:56 +0300

    Initial completion of bread crumb and full article page

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -2,7 +2,7 @@
 
 exports.home = async (req, res, next) => {
   try {
-    res.render('index', {title: 'SEB Base - Homepage', current: 'Home'});
+    res.render('index', {title: 'Home', current: 'Home', breadCrumb: 'Home'});
   } catch (error) {
     console.log(error);
   }
@@ -11,8 +11,9 @@ exports.home = async (req, res, next) => {
 exports.news = async (req, res, next) => {
   try {
     res.render('news', {
-      title: 'SEB Base - News',
+      title: 'News',
       current: 'News',
+      breadCrumb: 'News',
     });
   } catch (error) {
     console.log(error);
@@ -22,8 +23,21 @@ exports.news = async (req, res, next) => {
 exports.press = async (req, res, next) => {
   try {
     res.render('press', {
-      title: 'SEB Base - Press',
+      title: 'Press',
       current: 'Press',
+      breadCrumb: 'Press',
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.article = async (req, res, next) => {
+  try {
+    res.render('article', {
+      title: 'All of your necessities can be met with a loan',
+      current: 'News',
+      breadCrumb: 'News+All of your necessities can be met with a loan',
     });
   } catch (error) {
     console.log(error);
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const articleController = require('../controller/articleController');
+const articleController = require('../controllers/articleController');
 
 // Routes
 router.get('/', articleController.home);
 router.get('/news', articleController.news);
 router.get('/press', articleController.press);
+router.get('/news/article', articleController.article);
 
 module.exports = router;
