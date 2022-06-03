c16573dd393582f2815eca0ae1aaf4b13997ed38
Erikas <itserikasbykovskis@gmail.com>
2022-06-03 22:40:34 +0300

    Updated small color bug on small cards

--- public/js/script.js
+++ public/js/script.js
@@ -4,25 +4,30 @@ if (articleText !== null) articleText.innerHTML = articleText.textContent;
 
 // For toggling dark/light modes
 let mode = localStorage.getItem('mode');
+const chk = document.getElementById('chk');
 if (mode === null) {
   localStorage.setItem('mode', 'light');
   document.body.classList.remove('dark');
+  chk.checked = false;
 } else if (mode === 'dark') {
   localStorage.setItem('mode', 'dark');
   document.body.classList.add('dark');
+  chk.checked = true;
 } else {
   localStorage.setItem('mode', 'light');
   document.body.classList.remove('dark');
+  chk.checked = false;
 }
 
-const chk = document.getElementById('chk');
 chk.addEventListener('change', () => {
   let mode = localStorage.getItem('mode');
   if (mode === 'dark') {
     localStorage.setItem('mode', 'light');
     document.body.classList.remove('dark');
+    chk.checked = false;
   } else {
     localStorage.setItem('mode', 'dark');
     document.body.classList.add('dark');
+    chk.checked = true;
   }
 });
