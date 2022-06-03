c225ec68400d2001ec6b46600d4bea41b1255cf4
Erikas <itserikasbykovskis@gmail.com>
2022-06-03 22:29:26 +0300

    Made News, Press and Article mobile-friendly, also created dark-mode

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,28 @@
 // Set article main text as innerHTML, so <br> tags would work
 const articleText = document.querySelector('.article__text-p');
-articleText.innerHTML = articleText.textContent;
+if (articleText !== null) articleText.innerHTML = articleText.textContent;
+
+// For toggling dark/light modes
+let mode = localStorage.getItem('mode');
+if (mode === null) {
+  localStorage.setItem('mode', 'light');
+  document.body.classList.remove('dark');
+} else if (mode === 'dark') {
+  localStorage.setItem('mode', 'dark');
+  document.body.classList.add('dark');
+} else {
+  localStorage.setItem('mode', 'light');
+  document.body.classList.remove('dark');
+}
+
+const chk = document.getElementById('chk');
+chk.addEventListener('change', () => {
+  let mode = localStorage.getItem('mode');
+  if (mode === 'dark') {
+    localStorage.setItem('mode', 'light');
+    document.body.classList.remove('dark');
+  } else {
+    localStorage.setItem('mode', 'dark');
+    document.body.classList.add('dark');
+  }
+});
