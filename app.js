28470bab6833463316f84838e85f24af5259c47f
erikasby <94749107+erikasby@users.noreply.github.com>
2022-08-01 19:49:32 +0000

    Fully redesigned whole website

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -1,3 +1,23 @@
+const headings = [
+    'Control your environment.',
+    'Stable high performance.',
+    'Make your transactions secure.',
+    'Perform your task anywhere, anytime.',
+];
+const paragraphs = [
+    'Our plugin system give you ability to see and perform tasks how you want in many different ways.',
+    'Only 0.3% of the time in a year BayBank has low performance or is down completely. Other 99.7% of the time there is only pure speed!',
+    'All transactions have history of who, when, why and how they were made all the time. There is simply no possibility of your funds getting hijacked.',
+    'Want to get money transferred to your account on Sunday night in Seychelles? Let us make that happen in a matter of seconds.',
+];
+const img = ['index-1.avif', 'index-2.avif', 'index-3.avif', 'index-4.avif'];
+
+const cards = [];
+
+for (let i = 0; i < headings.length; i++) {
+    cards.push({heading: headings[i], paragraph: paragraphs[i], img: img[i]});
+}
+
 // GET
 // Home
 exports.home = async (req, res, next) => {
@@ -5,6 +25,7 @@ exports.home = async (req, res, next) => {
         res.render('index', {
             title: 'Home',
             path: '/',
+            cards: cards,
         });
     } catch (error) {
         res.render('404', {
